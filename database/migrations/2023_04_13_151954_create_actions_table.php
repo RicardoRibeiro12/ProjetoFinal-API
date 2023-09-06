f83788813d579c90c10940c5a2640f7f25af1f08
RodrigoGameiro <2191795@my.ipleiria.pt>
2023-09-06 18:21:18 +0100

    upadte actions migration

--- database/migrations/2023_04_13_151954_create_actions_table.php
+++ database/migrations/2023_04_13_151954_create_actions_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('actions', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('atuador_id');
-            $table->string('acao');
             $table->timestamps();
 
             $table->foreign('atuador_id')->references('id')->on('atuadores');
